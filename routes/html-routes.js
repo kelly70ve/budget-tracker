e7054e7b92c7b0cea50f86b5eb9f25b15891bf4d
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-07 23:27:08 -0700

    add path and status

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,9 @@
-const router = require("express").Router();
+const express = require('express');
+const path = require('path');
+const router = express.Router();
 
 router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.status(200).sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
