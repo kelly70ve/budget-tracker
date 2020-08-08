51aeff1da309a14b215b865133fa081913a37b9a
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-07 23:13:39 -0700

    add html route to home page

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ mongoose.connect(MONGODB_URI, {
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
