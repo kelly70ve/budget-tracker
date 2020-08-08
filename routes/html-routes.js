471aafeca0c42167f7e6ee43b862e6d6e30d8d3e
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-07 23:35:36 -0700

    remove html route

--- routes/html-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const router = express.Router();
-
-router.get("/", function(req, res) {
-  res.status(200).sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-module.exports = router;
