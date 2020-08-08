83a1628a6b89849ac763f7737a5c38ea36d75a97
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-08-07 23:00:55 -0700

    add mlab configuration

--- server.js
+++ server.js
@@ -15,10 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+var MONGODB_URI =  process.env.MONGODB_URI || "mongodb://user1:password1@ds339648.mlab.com:39648/heroku_x2hn8vwc";
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
 
 // routes
 app.use(require("./routes/api.js"));
